703247ecabbb97a84f52496ae138f2a8be4022ed
Baozi <baozi.rails@gmail.com>
2014-05-28 19:28:30 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railscrop::Application.config.secret_key_base = 'c09cce82b1f81fe54393676b7ace1f9183c6078adff0ad353510be4e048e97872d8b98bc7af8e213a8ba18c0f0912adca58f2ee417dae473fc6025efdce699c7'
